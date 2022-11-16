16d1a7de72d487058e2720282c90fc7a478e4cf2
Chris Garcia <garciac9203@gmail.com>
2022-11-15 17:15:32 -0800

    Finished assignment

--- datastore/counter.js
+++ datastore/counter.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const sprintf = require('sprintf-js').sprintf;
 
-var counter = 0;
-
 // Private helper functions ////////////////////////////////////////////////////
 
 // Zero padded numbers can only be represented as strings.
@@ -18,6 +16,7 @@ const zeroPaddedNumber = (num) => {
 const readCounter = (callback) => {
   fs.readFile(exports.counterFile, (err, fileData) => {
     if (err) {
+      console.log('What is the error: ', err);
       callback(null, 0);
     } else {
       callback(null, Number(fileData));
@@ -38,9 +37,14 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, fileData) => {
+    if (err) {
+      console.log('Error reading file getNextUniqueId');
+    } else {
+      writeCounter(fileData + 1, callback);
+    }
+  });
 };
 
 
--- datastore/index.js
+++ datastore/index.js
@@ -1,53 +1,142 @@
 const fs = require('fs');
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
+      console.log ('Error finding ID');
+    } else {
+      var newFileName = `${exports.dataDir}/${id}.txt`;
+      fs.writeFile(newFileName, text, (err) => {
+        if (err) {
+          console.log('Error, couldn\'t write file');
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  // var data = [];
+  // fs.readdir(exports.dataDir, (err, files) => {
+  //   if (err) {
+  //     console.log('Error in read all');
+  //   } else {
+  //     files.forEach((file) => {
+  //       var obj = {};
+  //       obj.id = file.split('.')[0];
+  //       obj.text = file.split('.')[0];
+  //       data.push(obj);
+  //     });
+  //     callback(null, data);
+  //   }
+  // });
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      console.log('Error in read all');
+    } else {
+      let data = _.map(files, (file) => {
+        let id = path.basename(file, '.txt');
+        let filepath = path.join(exports.dataDir, file);
+        return fs.readFileAsync(filepath)
+          .then(fileData => {
+            return {
+              id: id,
+              text: fileData.toString()
+            };
+          });
+      });
+      Promise.all(data)
+        .then(item => callback(null, item))
+        .catch(err => callback(err));
+    }
   });
-  callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  var text = null;
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      console.log('Error in readOne');
+    } else {
+      let foundFile = false;
+      files.forEach((file, index) => {
+        if (file.split('.')[0] === id) {
+          foundFile = true;
+          fs.readFile(`${exports.dataDir}/${file}`, (err, fileData) => {
+            if (err) {
+              console.log(err);
+            } else {
+              text = fileData.toString();
+              callback(null, { id, text });
+            }
+          });
+        }
+      });
+      if (!foundFile) {
+        callback(new Error(`No item with id: ${id}`));
+      }
+    }
+  });
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      console.log('Error in readOne');
+    } else {
+      let foundFile = false;
+      files.forEach((file, index) => {
+        if (file.split('.')[0] === id) {
+          foundFile = true;
+          fs.writeFile(`${exports.dataDir}/${file}`, text, (err, fileData) => {
+            if (err) {
+              console.log(err);
+            } else {
+              callback(null, { id, text });
+            }
+          });
+        }
+      });
+      if (!foundFile) {
+        callback(new Error(`No item with id: ${id}`));
+      }
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      console.log('Error in readOne');
+    } else {
+      let foundFile = false;
+      files.forEach((file, index) => {
+        if (file.split('.')[0] === id) {
+          foundFile = true;
+          fs.unlink(`${exports.dataDir}/${file}`, (err) => {
+            if (err) {
+              console.log('could not delete file');
+            } else {
+              callback();
+            }
+          });
+        }
+      });
+      if (!foundFile) {
+        callback(new Error(`No item with id: ${id}`));
+      }
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
